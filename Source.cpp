cfdc8b2da3891b2a8b50a4472f9e40e1ca762b1c
Ella Hoffman <e-hoffman.2@onu.edu>
2024-04-25 09:33:04 -0400

    Completed P12-1

--- Source.cpp
+++ Source.cpp
@@ -1,60 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
-#include <vector>
+#include <cctype>
 using namespace std;
 
 void pressEnterToContinue() {
-	cout << "\nPress enter to continue... \n";
-	cin.clear();
-	while (cin.get() != '\n') continue;
-}
-
-bool readFromCSVFile(string filename) {
-	ifstream inFile;
-	istringstream thisLine;
-	string movieInfo;
-	string movieTitle;
-	string year;
-	string director;
-
-	inFile.open(filename);
-	if (inFile.fail()) {
-		cout << "read error - sorry\n";
-		return false;
-	}
-
-	while (true) {
-		getline(inFile, movieInfo);
-		if (inFile.fail()) break;
-		thisLine.clear();
-		thisLine.str(movieInfo);
-		getline(thisLine, movieTitle, ',');
-		getline(thisLine, year, ',');
-
-		vector<string> directorList;
-		string dir;
-		while (getline(thisLine, dir, ','))
-			directorList.push_back(dir);
-
-		director = directorList[0];
-		for (int i = 1; i < directorList.size(); i++) {
-			director = director + " and " + directorList[i];
-		}
-		cout << movieTitle << " is a " << year << " film directed by " << director << endl;
-	}
-	inFile.close();
-	return true;
+    cout << "Press enter to continue...";
+    cin.get();
 }
 
 int main() {
-	string datafile;
-	cout << "Lab 12 Intro to Text Files!\n";
-	cout << "Enter filename: ";
-	getline(cin, datafile);
-	bool status = readFromCSVFile(datafile);
-	if (status) cout << "Success!\n";
-	else cout << "failure. :-(\n";
-	pressEnterToContinue();
+    string filename;
+    cout << "input file: ";
+    getline(cin, filename); 
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file. \n";
+        pressEnterToContinue();
+        return 1;
+    }
+
+    int wordCount = 0;
+    int charCount = 0;
+    string word;
+    while (file >> word) {
+        wordCount++;
+        for (char c : word) {
+            if (!isspace(c)) {
+                charCount++;
+            }
+        }
+    }
+
+    cout << "Words: " << wordCount << endl;
+    cout << "Characters: " << charCount << endl;
+    pressEnterToContinue();
+    return 0;
 }
